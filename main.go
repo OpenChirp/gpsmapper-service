ea776fff57df889520cb5d168547571f60ce1ead
Craig Hesling <craig@hesling.com>
2018-09-29 15:01:48 -0400

    Made os.Signal channel buffered

--- main.go
+++ main.go
@@ -402,7 +402,7 @@ func run(ctx *cli.Context) error {
 	log.Info("Published Service Status")
 
 	/* Setup signal channel */
-	signals := make(chan os.Signal)
+	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, os.Interrupt, syscall.SIGTERM)
 
 	/* Post service status indicating I started */
